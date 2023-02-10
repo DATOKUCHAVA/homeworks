2f1599b40754c478118e537d58ff63434b48de3f
DATOKUCHAVA <datokuchava6@gmail.com>
2023-02-10 15:32:21 +0400

    second commit

---
+++ day2/lesson2.py
@@ -0,0 +1,6 @@
+name = "hello my full  name is dato"
+age = 20 
+height =180
+surname = "kuchava am"
+address = "now i live in rustavi my height is "
+print(name + " " + surname + " " + str(age) + " " + address + " " +str(height))
